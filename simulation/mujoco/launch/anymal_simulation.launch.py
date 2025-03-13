f44967137ef3554a7851c8155f8c9db98aa52d7a
m.lisi1 <m.lisi1@studenti.unipi.it>
2025-03-13 15:11:29 +0100

    added anymal with modified inertia

--- simulation/mujoco/launch/anymal_simulation.launch.py
+++ simulation/mujoco/launch/anymal_simulation.launch.py
@@ -1,14 +1,12 @@
 import os
 from ament_index_python.packages import get_package_share_path
 from launch import LaunchDescription
-from launch.actions import DeclareLaunchArgument
-from launch.substitutions import LaunchConfiguration
 from launch_ros.actions import Node
 
 
 def generate_launch_description():
-    # xml_file_name = "model/xml/spot_mini/spot_mini.xml"
-    xml_file_name = "model/xml/anymal_c/scene.xml"
+
+    xml_file_name = "model/xml/anymal_c/scene_modified.xml"
     xml_file = os.path.join(get_package_share_path("description"), xml_file_name)
 
     return LaunchDescription(
@@ -20,13 +18,13 @@ def generate_launch_description():
                 output="screen",
                 parameters=[
                     {"simulation/model_file": xml_file},
-                    {"imu_timer":           2.5},
-                    {"joint_timer":         2.},
-                    {"odom_timer":          20.},
-                    {"sensor_odom_timer":   2.},
-                    {"touch_timer":         2.},
-                    {"img_timer":           20.},
-                    {"contacts_timer":      2.},
+                    {"imu_timer":           5},
+                    {"joint_timer":         5.},
+                    {"odom_timer":          5.},
+                    {"sensor_odom_timer":   5.},
+                    {"touch_timer":         5.},
+                    {"img_timer":           5.},
+                    {"contacts_timer":      5.},
                 ],
                 emulate_tty=True,
                 arguments=[("__log_level:=debug")],
